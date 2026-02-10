8a50cb7eea09c543bf15d2c0c9563680ca0b673f
srikantha357 <srikantha357@gmail.com>
2026-02-10 13:48:12 +0530

    Revert "Upgrade Offrecord Store with professional responsive UI and improved checkout UX"

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
+const CACHE_NAME = "offrecord-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/about.html",
+  "/shop.html",
+  "/cart.html",
+  "/contact.html",
+  "/account.html",
+  "/styles.css",
+  "/script.js"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });
