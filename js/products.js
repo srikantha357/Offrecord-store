7cb324b7a4bb9617cfc504b409ec677be5c322ff
srikantha357 <srikantha357@gmail.com>
2026-02-12 22:32:23 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,6 +6,6 @@ for (let i = 1; i <= 34; i++) {
     name: "OVERSIZED TEE " + i,
     price: 799,
     oldPrice: 999,
-    image: "assets/products/tee" + i + ".jpg"
+    image: "assets/products/tee" + i + ".webp"
   });
 }
