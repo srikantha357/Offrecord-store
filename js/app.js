119786ea34c4b2dc9049451df9c7a1228c6aae1f
srikantha357 <srikantha357@gmail.com>
2026-02-12 20:12:52 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -55,3 +55,12 @@ function updateCartCount() {
 loadProducts();
 updateCartCount();
 
+// HERO SLIDER
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}, 4000);
