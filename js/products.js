8303f2c2d9c402445763cdb3cd77958fe5c7c236
srikantha357 <srikantha357@gmail.com>
2026-02-13 00:34:13 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,6 +6,6 @@ for (let i = 1; i <= 34; i++) {
     name: "OVERSIZED TEE " + i,
     price: 799,
     oldPrice: 999,
-    image: "https://cdn.jsdelivr.net/gh/srikantha357/Offrecord-store@main/assets/tee" + i + ".webp"
+    image: "https://cdn.jsdelivr.net/gh/srikantha357/Offrecord-store@main/assets/tee" + i + ".webp?v=1"
   });
 }
